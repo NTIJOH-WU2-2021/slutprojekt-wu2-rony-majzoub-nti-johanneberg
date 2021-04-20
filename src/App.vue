58675a93982ef49adac0851ebee2f67cfc329762
rony-majzoub-nti-johanneberg <60172919+rony-majzoub-nti-johanneberg@users.noreply.github.com>
2021-04-20 08:29:42 +0200

    Imported Previous Repo

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <IngredientSearch/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import IngredientSearch from './components/IngredientSearch.vue'
+
+export default {
+  name: 'App',
+  components: { HelloWorld, IngredientSearch }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
+#app {
+  font-family: 'Inter', 'Lato', Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: rgba(0, 0, 0, 0.87);
+  background-color: #F2F2F7;
+}
+body {
+  background-color: #F2F2F7;
+}
+</style>
---
+++ src/components/IngredientSearch.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="flex">  
+    <input
+      data-cy="city"
+      class="input"
+      type="search"
+      placeholder="Köttkulla.."
+      @keyup.enter="onSearchClick"
+      v-model="search"
+    />
+    <button @click="onSearchClick" class="search">
+      <img src="..\assets\search-24px.svg" alt="">
+    </button>
+  </div>
+</template>
+
+<script>
+// import config from '@/appConfig.js';
+export default {
+  name: "IngredientSearch",
+}
+</script>
+
+<style>
+.flex {
+  display: flex;
+  max-width: 400px;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 1em;
+  gap: 0.5rem;
+}
+
+.input {
+  width: 100%;
+  padding: 8px 10px;
+  border: none;
+  flex: 1;
+  background-color: #F6F6F6;
+  border-radius: 100px;
+  font-family: 'Inter', 'Lato', Avenir, Helvetica, Arial, sans-serif;
+  color: rgba(0, 0, 0, 0.60);
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 19px;
+  height: 48px;
+  box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.14), 0px 2px 1px rgba(0, 0, 0, 0.12), 0px 1px 3px rgba(0, 0, 0, 0.2);
+}
+
+.search {
+  background-color: transparent;
+  border: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  height: 48px;
+  width: 48px;
+}
+img {
+  height: 36px;
+  color: #666666;
+}
+</style>
